df3038f5f0d8197288725d264d62c940bd94a0b1
Izzo <izzoy@wit.edu>
2022-04-28 21:54:54 -0400

    Performance improvements

--- MarchingCubes.gd
+++ MarchingCubes.gd
@@ -41,64 +41,57 @@ func generate():
 	for x in range(grid_size.x-1):
 		for y in range(grid_size.y-1):
 			for z in range(grid_size.z-1):
-				build_cell(x,y,z,st)
+				# This used to be a function call, let's see if moving it here is cheaper
+				# Get cube indicex
+				var cube_indices = [
+					int((x)+((z+1)*grid_size.x)+((y)*grid_slice_size)),
+					int((x+1)+((z+1)*grid_size.x)+((y)*grid_slice_size)),
+					int((x+1)+((z)*grid_size.x)+((y)*grid_slice_size)),
+					int((x)+((z)*grid_size.x)+((y)*grid_slice_size)),
+					int((x)+((z+1)*grid_size.x)+((y+1)*grid_slice_size)),
+					int((x+1)+((z+1)*grid_size.x)+((y+1)*grid_slice_size)),
+					int((x+1)+((z)*grid_size.x)+((y+1)*grid_slice_size)),
+					int((x)+((z)*grid_size.x)+((y+1)*grid_slice_size)),
+				]
+				
+				# Build triangulation table index
+				var index = 0
+				for i in range(8):
+					if grid[cube_indices[i]] <= surface_level:
+						index |= (1 << i)
+				
+				if McTables.Edges[index] == 0:
+					# This cube is either totally inside or outside the surface, ignore it
+					continue
+				
+				var cube_vertices = [
+					Vector3(x,y,z+1)*cell_size,
+					Vector3(x+1,y,z+1)*cell_size,
+					Vector3(x+1,y,z)*cell_size,
+					Vector3(x,y,z)*cell_size,
+					Vector3(x,y+1,z+1)*cell_size,
+					Vector3(x+1,y+1,z+1)*cell_size,
+					Vector3(x+1,y+1,z)*cell_size,
+					Vector3(x,y+1,z)*cell_size
+				]
+				
+				# Build edge list
+				var triangulation = McTables.Triangulation[index]
+				for edgeIndex in triangulation:
+					if edgeIndex == -1:
+						break
+					var edges = McTables.VerticesFromEdge[edgeIndex]
+					
+					var vertex = (cube_vertices[edges[0]] + cube_vertices[edges[1]]) / 2
+					#var p1 = cube_vertices[edges[0]]
+					#var p2 = cube_vertices[edges[1]]
+					#var v1 = grid[cube_indices[edges[0]]]
+					#var v2 = grid[cube_indices[edges[1]]]
+					#var vertex = p1+(((surface_level-v1)*(p2-p1))/(v2-v1))
+					
+					st.add_vertex(vertex)
 	
 	# Actually turn into a mesh
 	st.generate_normals()
 	var array_mesh = st.commit()
 	mesh = array_mesh
-	yield(get_tree(),'idle_frame')
-
-
-func build_cell(x:int,y:int,z:int,st:SurfaceTool):
-	
-	# Get cube indicex
-	var cube_indices = [
-		coord2index(x,y,z+1),
-		coord2index(x+1,y,z+1),
-		coord2index(x+1,y,z),
-		coord2index(x,y,z),
-		coord2index(x,y+1,z+1),
-		coord2index(x+1,y+1,z+1),
-		coord2index(x+1,y+1,z),
-		coord2index(x,y+1,z),
-	]
-	
-	# Build triangulation table index
-	var index = 0
-	for i in range(8):
-		if grid[cube_indices[i]] <= surface_level:
-			index |= (1 << i)
-	
-	if McTables.Edges[index] == 0:
-		# This cube is either totally inside or outside the surface, ignore it
-		return false
-	
-	var cube_vertices = [
-		Vector3(x,y,z+1)*cell_size,
-		Vector3(x+1,y,z+1)*cell_size,
-		Vector3(x+1,y,z)*cell_size,
-		Vector3(x,y,z)*cell_size,
-		Vector3(x,y+1,z+1)*cell_size,
-		Vector3(x+1,y+1,z+1)*cell_size,
-		Vector3(x+1,y+1,z)*cell_size,
-		Vector3(x,y+1,z)*cell_size
-	]
-	
-	# Build edge list
-	var triangulation = McTables.Triangulation[index]
-	for edgeIndex in triangulation:
-		if edgeIndex == -1:
-			break
-		var edges = McTables.VerticesFromEdge[edgeIndex]
-		
-		#var vertex = (cube_vertices[edges[0]] + cube_vertices[edges[1]]) / 2
-		var p1 = cube_vertices[edges[0]]
-		var p2 = cube_vertices[edges[1]]
-		var v1 = grid[cube_indices[edges[0]]]
-		var v2 = grid[cube_indices[edges[1]]]
-		var vertex = p1+(((surface_level-v1)*(p2-p1))/(v2-v1))
-		
-		st.add_vertex(vertex)
-	
-	return true
